ca0f77935a2fafb0dae375923a717dd4e04c3df4
Jose Alberto <jacosta_21@alu.uabcs.mx>
2024-11-21 11:27:43 -0700

    pf-avance1

---
+++ Proyecto_Final/layouts/header.php
@@ -0,0 +1,13 @@
+<header>
+    <div class="box-ejemplo logo">
+        <img class="logo-image" src="img/logo.png">
+    </div>
+    <nav>
+        <ul>
+            <li><a class="navigation-link" href="#">RED UABCS</a></li>
+            <li><a href="../Proyecto_Final/index.html">HOME</a></li>
+            <li><a href="https://www.uabcs.mx">UABCS</a></li>
+            <li><a href="https://siia-web.uabcs.mx">SIIA</a></li>
+        </ul>
+    </nav>
+</header>
