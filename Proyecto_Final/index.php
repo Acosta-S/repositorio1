336f46d5e48350cce5c2300c62ee2d0da7e599f3
Acosta-S <jacosta_21@alu.uabcs.mx>
2024-12-02 20:27:20 -0700

    website-pf1

---
+++ Proyecto_Final/index.php
@@ -0,0 +1,23 @@
+<!-- PAGINA PRINCIPAL RED UABCS -->
+  <!-- Barra de navegación -->
+<?php include_once '/../src/views/layouts/header.php';?>
+
+<main>
+    <section class="seccion-tareas">
+        <div class="caja">
+        <p>Hello world</p>
+
+        
+        </div>
+    </section>
+
+    <section class="seccion-material">
+        <div class="caja">
+        <p>Figura1</p>
+        
+
+        </div>
+    </section>
+
+</main>
+<?php include '../src/views/layouts/footer.php'; ?>
